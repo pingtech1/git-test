027543f8617d5806747c9cf64059a6b71973c7a7
admin <654573003@qq.com>
2019-04-03 19:18:31 +0800

    第二次修改

--- fristgit/src/main/java/GItTest.java
+++ fristgit/src/main/java/GItTest.java
@@ -2,11 +2,30 @@ public class GItTest {
 
 
     public static void main(String[] args) {
+
         System.out.println("第一次上传");
+        // hello test
     }
 
 
     public void test(){
         int i = 0;
+
+        System.out.println(i+10);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
